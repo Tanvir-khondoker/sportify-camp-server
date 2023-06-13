f009ad6889bb92ccfe188821e5926a57c7eb399c
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-06-13 09:32:23 +0600

    jwt installed

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akiedbb.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,92 +22,84 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
     await client.connect();
 
     const usersCollection = client.db("sportifyDb").collection("users");
     const CoursesCollection = client.db("sportifyDb").collection("Courses");
     const cartCollection = client.db("sportifyDb").collection("carts");
 
-    
-    // api to get all the courses
-    app.get('/courses', async(req, res)=>{
-        const result = await CoursesCollection.find().toArray();
-        res.send(result);
-    })
-  
+    // API to get all the courses
+    app.get('/courses', async (req, res) => {
+      const result = await CoursesCollection.find().toArray();
+      res.send(result);
+    });
 
-    // users related api 
-    app.get('/users', async(req, res)=>{
+    // Users related APIs
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/users', async(req, res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log(user);
-      const query = {email: user.email}
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-      console.log('existing user :', existingUser);
-      if(existingUser){
-        return res.send({message: 'user already exist' })
+      if (existingUser) {
+        return res.send({ message: 'User already exists' });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
-    
-    // make admin api
-    app.patch('/users/admin/:id', async(req, res)=>{
-       const id = req.params.id;
-       const filter = {_id: new ObjectId(id)};
-       const updateDoc ={
-        $set:{
+    });
+
+    // Make admin API
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
           role: 'admin'
-        },
-       };
-       const result = await usersCollection.updateOne(filter, updateDoc);
-       res.send(result);
-    })
-    // make instructor api
-    app.patch('/users/instructor/:id', async(req, res)=>{
-       const id = req.params.id;
-       const filter = {_id: new ObjectId(id)};
-       const updateDoc ={
-        $set:{
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // Make instructor API
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
           role: 'instructor'
-        },
-       };
-       const result = await usersCollection.updateOne(filter, updateDoc);
-       res.send(result);
-    })
- 
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-
-
-  // classes related api, cart collection
-  app.get('/carts', async(req, res)=>{
+    // Classes related APIs, cart collection
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([]);
       }
-      const query = {email:email};
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-  });
-  app.post('/carts' ,async(req, res)=>{
-    const item = req.body;
-    const result = await cartCollection.insertOne(item);
-    res.send(result);
-  })
+    });
 
-  app.delete('/carts/:id', async(req, res)=>{
-    const id = req.params.id;  
-    const query = {_id: new ObjectId(id)};
-    const result = cartCollection.deleteOne(query);
-    res.send(result);
-  })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
 
-
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -122,25 +109,13 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+  res.send("Sportify is running");
+});
 
-
-
-
-
-
-
-app.get('/', (req, res)=>{
-    res.send("spotify is running")
-})
-
-
-app.listen(port, ()=>{
-    console.log(`SportifyCamp is spinning on port ${port}`);
-})
-
-
-
-
-
+app.listen(port, () => {
+  console.log(`Sportify is spinning on port ${port}`);
+});
