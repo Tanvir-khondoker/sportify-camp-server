39df618429d3ae57240307db5058ed509ede2337
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-06-14 09:20:00 +0600

    update i dont remember  where i stopped as i have exam going on in university

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next)=>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true, message:'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error:true, message:'unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akiedbb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,7 +49,7 @@ async function run() {
 
     app.post('/jwt', (req, res)=>{
       const user = req.body;
-      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '2h' })
       res.send({token})
     })
 
@@ -85,11 +103,17 @@ async function run() {
     });
 
     // Classes related APIs, cart collection
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email != decodedEmail){
+        return res.status(403).send({error:true, message:"forbidden access"})
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
