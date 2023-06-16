79591f19d122bd49546e5c6ef283e20cff4b028a
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2023-06-16 22:38:01 +0600

    almost done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -26,6 +27,25 @@ const verifyJWT = (req, res, next)=>{
   })
 }
 
+// admin ony and and instructor only link secure
+const verifyAdmin = async(req, res, next)=>{
+  const email = req.decoded.email;
+  const query = {email:email}
+  const user = await usersCollection.findOne(query);
+  if(user?.role == 'admin'){
+    return res.status(403).send({error:true, message: 'forbidden message'});
+  }
+}
+const verifyInstructor = async(req, res, next)=>{
+  const email = req.decoded.email;
+  const query = {email:email}
+  const user = await usersCollection.findOne(query);
+  if(user?.role == 'instructor'){
+    return res.status(403).send({error:true, message: 'forbidden message'});
+  }
+  next();
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akiedbb.mongodb.net/?retryWrites=true&w=majority`;
@@ -46,6 +66,7 @@ async function run() {
     const usersCollection = client.db("sportifyDb").collection("users");
     const CoursesCollection = client.db("sportifyDb").collection("Courses");
     const cartCollection = client.db("sportifyDb").collection("carts");
+    const paymentCollection = client.db("sportifyDb").collection("payments");
 
     app.post('/jwt', (req, res)=>{
       const user = req.body;
@@ -53,15 +74,37 @@ async function run() {
       res.send({token})
     })
 
-    // API to get all the courses
+    // API to get all the courses that's status approved
     app.get('/courses', async (req, res) => {
+      const result = await CoursesCollection.find({ status: "approved" }).toArray();
+      res.send(result);
+    });
+
+
+    // api to get all the courses that are approved or not
+    app.get('/allCourses', async (req, res) => {
       const result = await CoursesCollection.find().toArray();
       res.send(result);
     });
 
+
+    // api to post a new corse/class by instructor
+    app.post('/courses', async(req, res)=>{
+       const newClass = req.body
+       const result = await CoursesCollection.insertOne(newClass);
+       res.send(result);
+    })
+    
+
     // Users related APIs
-    app.get('/users', async (req, res) => {
+    app.get('/users',  async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // instructor ge api
+    app.get('/teachers', async (req, res) => {
+      const result = await usersCollection.find({ role: "instructor" }).toArray();
       res.send(result);
     });
 
@@ -134,6 +177,49 @@ app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+   
+
+  //  api to approve deny and feedback related
+  //approved 
+  app.patch('/allCourseS/approved/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        status: 'approved'
+      }
+    };
+    const result = await CoursesCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+  // denied
+  app.patch('/allCourseS/denied/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        status: 'denied'
+      }
+    };
+    const result = await CoursesCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+  // api to feedback 
+  app.patch('/allCourseS/feedback/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        feedback: req.body.feedback
+      }
+    };
+    const result = await CoursesCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+
+
+
 
 
   // get all the instructors api 
@@ -173,6 +259,51 @@ app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
       const result = cartCollection.deleteOne(query);
       res.send(result);
     });
+
+     // Get classes for a specific instructor
+  app.get('/classes/instructor/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+
+    try {
+      const result = await CoursesCollection.find(query).toArray();
+      res.send(result);
+    } catch (error) {
+      console.error('Error retrieving classes:', error);
+      res.status(500).send({ error: true, message: 'Internal server error' });
+    }
+  });
+    
+
+
+    // create payment intent
+    app.post('/create-payment-intent', async(req, res)=>{
+         const {price} = req.body;
+         const amount = price*100;
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount : amount,
+            currency: 'usd',
+            payment_method_types: ['card']
+         });
+
+         res.send({
+          clientSecret: paymentIntent.client_secret
+         })
+    })
+
+    // payment related api
+    app.post('/payments', async(req, res)=>{
+      const payment = req.body;
+      console.log('payment:',payment);
+      const insertResult = await paymentCollection.insertOne(payment);
+
+    const query = {_id:{$in:payment.cartItems.map(item => new ObjectId(item))}}
+    const deleteResult = await cartCollection.deleteMany(query)
+
+
+      res.send({insertResult, deleteResult});
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
